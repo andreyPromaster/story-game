55af5324b5e794ebaf6e6cc6a25043a35cb5fcf8
andreyPromaster <and45.rew@ya.ru>
2021-10-13 15:44:24 +0300

    Fix after review

--- entities/connection.py
+++ entities/connection.py
@@ -5,16 +5,17 @@ from boto3.dynamodb.conditions import Key
 
 from conf import settings
 from entities.schemas import Node, Story
+from utilities.exceptions import DynamoDBError
 
 
 class DataDriver(abc.ABC):
     @abc.abstractmethod
     def get_story(self, story_id: str):
-        pass
+        """ "Get story from different source by story_id"""
 
     @abc.abstractmethod
     def get_node(self, story_id: str, uri: str):
-        pass
+        """Get story node from source by story_id and nodes URI"""
 
 
 class DynamoDBDriver(DataDriver):
@@ -27,7 +28,7 @@ class DynamoDBDriver(DataDriver):
             ProjectionExpression="id, root",
         )
         if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-            raise Exception(json.dumps(response))
+            raise DynamoDBError(json.dumps(response))
 
         if response["Count"] == 1:
             return Story(**response["Items"][0])
@@ -39,7 +40,9 @@ class DynamoDBDriver(DataDriver):
             ProjectionExpression=f"nodes.{uri}",
         )
         if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-            raise Exception(json.dumps(response))
+            raise DynamoDBError(json.dumps(response))
 
         if response["Count"] == 1:
             return Node(**response["Items"][0]["nodes"][uri])
+        else:
+            return {}
