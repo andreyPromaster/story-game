71e37afd44e0a62ad9e7cfa4f75d24c2ab629c1d
andreyPromaster <and45.rew@ya.ru>
2021-10-13 13:40:08 +0300

    Fix code style

--- app.py
+++ app.py
@@ -9,14 +9,18 @@ def test():
     conf = Config(retries={"max_attempts": 1, "mode": "standard"})
 
     conn = boto3.resource(
-        "dynamodb", endpoint_url=f"http://{settings.DYNAMODB_HOST}:{settings.DYNAMODB_PORT}", config=conf,
-        region_name=settings.REGION)
+        "dynamodb",
+        endpoint_url=f"http://{settings.DYNAMODB_HOST}:{settings.DYNAMODB_PORT}",
+        config=conf,
+        region_name=settings.REGION,
+    )
 
     source = DynamoDBDriver(conn)
     node = source.get_node("test", "Root")
     print(node)
     story = source.get_story("test")
     print(story)
+
 
 if __name__ == "__main__":
     test()
--- entities/connection.py
+++ entities/connection.py
@@ -1,7 +1,7 @@
 import abc
 import json
 
-from boto3.dynamodb.conditions import Key, AttributeExists
+from boto3.dynamodb.conditions import Key
 
 from conf import settings
 from entities.schemas import Node, Story
@@ -22,8 +22,10 @@ class DynamoDBDriver(DataDriver):
         self.connection = connection.Table(settings.STORY_TABLE_NAME)
 
     def get_story(self, story_id: str):
-        response = self.connection.query(KeyConditionExpression=Key("id").eq(story_id),
-                                         ProjectionExpression=f"id, root",)
+        response = self.connection.query(
+            KeyConditionExpression=Key("id").eq(story_id),
+            ProjectionExpression="id, root",
+        )
         if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
             raise Exception(json.dumps(response))
 
@@ -31,10 +33,11 @@ class DynamoDBDriver(DataDriver):
             return Story(**response["Items"][0])
 
     def get_node(self, story_id: str, uri: str):
-        response = self.connection.query(KeyConditionExpression=Key("id").eq(story_id),
-                                         FilterExpression=f"attribute_exists(nodes.{uri})",
-                                         ProjectionExpression=f"nodes.{uri}",
-                                         )
+        response = self.connection.query(
+            KeyConditionExpression=Key("id").eq(story_id),
+            FilterExpression=f"attribute_exists(nodes.{uri})",
+            ProjectionExpression=f"nodes.{uri}",
+        )
         if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
             raise Exception(json.dumps(response))
 
