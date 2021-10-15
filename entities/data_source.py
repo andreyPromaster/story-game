f5cb09bd4c193fb73fb1037a3be9e3399686dcb3
andreyPromaster <and45.rew@ya.ru>
2021-10-15 10:23:18 +0300

    Fix typo

--- entities/data_source.py
+++ entities/data_source.py
@@ -25,11 +25,11 @@ def _get_connection():
 class DataDriver(abc.ABC):
     @abc.abstractmethod
     def get_story(self, story_id: str):
-        """ "Get story_api from different source by story_id"""
+        """ "Get story from different source by story_id"""
 
     @abc.abstractmethod
     def get_node(self, story_id: str, uri: str):
-        """Get story_api node from source by story_id and nodes URI"""
+        """Get story node from source by story_id and nodes URI"""
 
     @abc.abstractmethod
     def get_story_list(self):
