01419b69c7bb403177113853e9c2e32389d71c1c
andreyPromaster <and45.rew@ya.ru>
2021-10-15 10:21:55 +0300

    Create api to get story and nodes

--- conf.py
+++ conf.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     DYNAMODB_HOST: str = "127.0.0.1"
     DYNAMODB_PORT: int = 8000
     STORY_TABLE_NAME: str = "stories"
+    DYNAMODB_URL: str = f"http://{DYNAMODB_HOST}:{DYNAMODB_PORT}"
+    DATA_SOURCE: str = "dynamodb"
 
 
 settings = Settings()
--- entities/data_source.py
+++ entities/data_source.py
@@ -1,21 +1,39 @@
 import abc
 import json
 
+import boto3
 from boto3.dynamodb.conditions import Key
+from botocore.config import Config
 
 from conf import settings
 from entities.schemas import Node, Story
 from utilities.exceptions import DynamoDBError
 
 
+def _get_connection():
+    conf = Config(retries={"max_attempts": 1, "mode": "standard"})
+
+    conn = boto3.resource(
+        "dynamodb",
+        endpoint_url=settings.DYNAMODB_URL,
+        config=conf,
+        region_name=settings.REGION,
+    )
+    return conn
+
+
 class DataDriver(abc.ABC):
     @abc.abstractmethod
     def get_story(self, story_id: str):
-        """ "Get story from different source by story_id"""
+        """ "Get story_api from different source by story_id"""
 
     @abc.abstractmethod
     def get_node(self, story_id: str, uri: str):
-        """Get story node from source by story_id and nodes URI"""
+        """Get story_api node from source by story_id and nodes URI"""
+
+    @abc.abstractmethod
+    def get_story_list(self):
+        """Get list of stories"""
 
 
 class DynamoDBDriver(DataDriver):
@@ -46,3 +64,16 @@ class DynamoDBDriver(DataDriver):
         if response["Count"] == 1:
             return Node(**response["Items"][0]["nodes"][uri])
         return None
+
+    def get_story_list(self):
+        response = self.connection.scan(
+            ProjectionExpression="id, root",
+        )
+        if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+            raise DynamoDBError(json.dumps(response))
+        return response["Items"]
+
+
+def get_data_source() -> DataDriver:
+    if settings.DATA_SOURCE == "dynamodb":
+        return DynamoDBDriver(_get_connection())
