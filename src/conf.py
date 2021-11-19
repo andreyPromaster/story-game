d8e00a6c21331effc1ef79de665d4b37c4915a43
andreyPromaster <ohotnik17.com@gmail.com>
2021-11-19 17:44:05 +0300

    Add init migration, fix models

--- src/conf.py
+++ src/conf.py
@@ -3,10 +3,17 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     REGION: str = "us-east-1"
-    STORY_TABLE_NAME: str = "stories"
+    STORY_TABLE_NAME_DYNAMODB: str = "stories"
     DYNAMODB_URL: str = "http://127.0.0.1:8000"
     DATA_SOURCE: str = "dynamodb"
     LOCAL: bool = True
+
+    # RDS setting
+    DB_USER: str = "postgres"
+    DB_HOST: str = ""
+    DB_PORT: int = 5432
+    DB_NAME: str = "game_story"
+    DB_PASS: str = ""
 
 
 settings = Settings()
--- src/data_storage/sqlalchemy/models.py
+++ src/data_storage/sqlalchemy/models.py
@@ -1,36 +1,66 @@
-from sqlalchemy import NVARCHAR, Column, ForeignKey, Integer
+import json
+import logging
+from functools import partial
+
+from pydantic.json import pydantic_encoder
+from sqlalchemy import VARCHAR, Column, ForeignKey, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# if NOT_A_LAZY_CONNECTION:
-#     engine = get_connection(tunnelled=USE_RDS_PROXY)
-#     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# else:
-#     engine = LazyConnection(tunnelled=USE_RDS_PROXY)
-#     SessionLocal = None
+from conf import settings
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def get_connection():
+    # Setup Session and Client
+    logging.info("Getting database connection")
+    conn = create_engine(
+        f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}",
+        json_serializer=partial(json.dumps, default=pydantic_encoder),
+    )
+    logging.info("Database connection established")
+    return conn
+
+
+engine = get_connection()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base(bind=engine)
 
 
 class Story(Base):
     __tablename__ = "story"
+
+    id = Column(Integer, primary_key=True)
     root = Column(
-        Integer, ForeignKey("node.id", ondelete="CASCADE"), nullable=False, index=True
+        Integer,
+        ForeignKey("node.id", ondelete="CASCADE", use_alter=True),
+        nullable=False,
+        index=True,
     )
-    name = Column(NVARCHAR(200), nullable=False)
+    name = Column(VARCHAR(200), nullable=False)
 
 
 class Node(Base):
     __tablename__ = "node"
+
+    id = Column(Integer, primary_key=True)
     story = Column(
-        Integer, ForeignKey("story.id", ondelete="CASCADE"), nullable=False, index=True
+        Integer,
+        ForeignKey("story.id", ondelete="CASCADE", use_alter=True),
+        nullable=False,
+        index=True,
     )
-    name = Column(NVARCHAR(200), nullable=False)
-    text = Column(NVARCHAR(200), nullable=False)
+    name = Column(VARCHAR(200), nullable=False)
+    text = Column(VARCHAR(200), nullable=False)
 
 
 class Option(Base):
     __tablename__ = "option"
+
+    id = Column(Integer, primary_key=True)
     next = Column(
         Integer, ForeignKey("node.id", ondelete="CASCADE"), nullable=False, index=True
     )
-    text = Column(NVARCHAR(200), nullable=False)
+    text = Column(VARCHAR(200), nullable=False)
