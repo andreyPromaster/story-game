33b0395d865d2e1c988515789fcc8c0e2ffa19b0
andreyPromaster <ohotnik17.com@gmail.com>
2021-12-27 18:57:00 +0300

    Create rds test data and change dynamodb test data

---
+++ tests/test_data.sql
@@ -0,0 +1,8 @@
+insert into story values (1, 'test-story');
+insert into node values (1, 1, 'Root', 'root');
+insert into node values (2, 1, 'Root1', 'root1');
+
+insert into option values (1, 2, 1, 'br1');
+insert into option values (2, null, 1, 'br2');
+insert into option values (3, null, 2, 'br3');
+insert into option values (4, null, 2, 'br4');
