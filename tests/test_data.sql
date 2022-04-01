248d910b2272653a20317f77edc4af024cc41b35
andreyPromaster <ohotnik17.com@gmail.com>
2022-04-01 13:38:27 +0300

    Refactoring sql data uploadint, refactoring

--- tests/test_data.sql
+++
@@ -1,9 +0,0 @@
-insert into story values (1, 'test_story');
-insert into node values (DEFAULT, 1, 'Root', 'root'), (DEFAULT, 1, 'Root1', 'root1');
-insert into story_root (id, story, node) values (DEFAULT,1,1);
-insert into option values (DEFAULT, 2, 1, 'br1'), (DEFAULT, null, 1, 'br2'),
-                          (DEFAULT, null, 2, 'br3'), (DEFAULT, null, 2, 'br4');
-
--- SELECT setval(pg_get_serial_sequence('story', 'id'), MAX(id)) FROM story;
--- SELECT setval(pg_get_serial_sequence('node', 'id'), MAX(id)) FROM node;
--- SELECT setval(pg_get_serial_sequence('option', 'id'), MAX(id)) FROM option;
